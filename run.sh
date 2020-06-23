9739a5e2fd1ab23200f6191c27d58e5a7598d610
FedorSarafanov <sfg180@yandex.ru>
2020-06-23 15:16:06 +0300

    New run

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
-cd ~/BoltekData;
-ps -C get_data >/dev/null && echo "" || exec ~/BoltekData/get_data &
+list=$(pidof -o %PPID get_data)
+
+i=0
+for PID in $list
+do
+	echo "Killing $PID...";
+	p=`ls -ald --color=never /proc/$PID/exe | awk '{ print $11 }'`
+	if [[ $p == *"BoltekData"* ]]; then
+		((i=i+1))
+	fi
+done
+
+if [ "$i" -eq "0" ]; then
+	cd ~/BoltekData;
+   exec ~/BoltekData/get_data;
+fi
