04afac93c8b7f1cd8827d62c440ab7be693f6fc7
FedorSarafanov <sfg180@yandex.ru>
2021-02-19 13:59:32 +0300

    test

--- lastplot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-file=$(ls -1t | grep .txt | head -n 2)
-python3 load.py $file
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ~/BoltekData;
-ps -C get_data >/dev/null && echo "" || exec ~/BoltekData/get_data &
